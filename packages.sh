0205e8d8aaccdbbea863e3891b40e5a499052a58
zlw555 <57552656+zlw555@users.noreply.github.com>
2024-03-27 23:30:19 +0800

    z

--- packages.sh
+++ packages.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
+echo "搜索.DS_Store文件并刪除它們"
+find ./ -iname ".DS_Store" -exec rm {}  \;
+echo ".DS_Store 文件已刪除"
+echo "开始重新压缩deb"
 rm -rf Packages Packages.xz Packages.gz Packages.bz2 Packages.lzma Packages.zst
 dpkg-scanpackages -m ./debs /dev/null >Packages
 xz -c9 Packages > Packages.xz
 gzip -c9 Packages > Packages.gz
 bzip2 -c9 Packages > Packages.bz2
 lzma -c9 Packages > Packages.lzma
-zstd -q -c19 Packages > Packages.zst
-find ./ -name ".DS_Store" -depth -exec rm {} \;
